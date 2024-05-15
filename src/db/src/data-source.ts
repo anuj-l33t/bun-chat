f5ca5307149fe60df70dd535c9f864f2726fc690
Mihir Patel <mpatel88@gmail.com>
2024-05-15 07:26:09 -0700

    Removing database from repo Adding PGAdmin and App dev environment to docker Using named volumes for PGAdmin and Postgres in docker Cleaning up extraneous README, tsconfig, package.json, and .gitignore files Moving TypeORM Datasource initialization to the API server's index.ts

--- src/db/src/data-source.ts
+++ src/db/src/data-source.ts
@@ -4,13 +4,15 @@ import { Users } from "./entity/Users"
 import { Groups } from "./entity/Groups"
 import { Messages } from "./entity/Messages"
 
+const postgresPassword = await Bun.file(process.env.POSTGRES_PASSWORD_FILE ?? "").text();
+
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "psqldb",
     port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "chat-app-db",
+    username: process.env.POSTGRES_USER,
+    password: postgresPassword,
+    database: process.env.POSTGRES_DB,
     synchronize: true,
     logging: false,
     entities: [
